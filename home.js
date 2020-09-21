e103b2e9ac763c498331854b755ad1d4a1d1f828
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-09-21 17:40:16 +0200

    meta test 2

--- home.js
+++ home.js
@@ -3,7 +3,4 @@ let info, head = d3.select('head');
 d3.text('info.yml').then(yml=>{
     info = jsyaml.load(yml);
 
-    const metaTags = head.selectAll('meta').data(info.meta).enter().append('meta')
-        .attr('property',d=>d.property)
-        .attr('content',d=>d.content)
 })
--- meta.js
+++
@@ -1,8 +0,0 @@
-let head = d3.select('head');
-d3.text('info.yml').then(info=>{
-    info = jsyaml.load(info);
-
-    const metaTags = head.selectAll('meta').data(info.meta).enter().append('meta')
-        .attr('property',d=>d.property)
-        .attr('content',d=>d.content)
-})
