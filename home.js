d4861680b9bc2d5d7889dc4e965fecc88035c854
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-09-21 18:06:52 +0200

    init

--- home.js
+++ home.js
@@ -1,6 +1,18 @@
 // This script generates HTML nodes for the home page of the project
-let info, head = d3.select('head');
-d3.text('info.yml').then(yml=>{
-    info = jsyaml.load(yml);
+const questions = d3.select('#questions');
 
-})
+Promise.all([
+    d3.text('info.yml'),
+    d3.text('questions.yml'),
+  ])
+  .then(([info, questionsData]) =>  {
+    info = jsyaml.load(info);
+    questionsData = jsyaml.load(questionsData);
+
+    const question = questions.selectAll('div').data(questionsData).enter().append('div');
+    question.append('h1').text(d=>d.title);
+    question.append('p').text(d=>d.description);
+    question.append('a').attr('href',d=>`./${d.folder}`).text('go to question');
+    question.append('img').attr('src',d=>`./${d.folder}/${d.cover}`);
+
+  });
