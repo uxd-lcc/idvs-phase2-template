ca2d8ec05798acbbf6075e17e184e27d893fbffa
bndndr <benedetti.a93@gmail.com>
2020-10-12 16:53:32 +0200

    templating

--- load-templates.js
+++ load-templates.js
@@ -1,13 +1,33 @@
 // This script generates HTML nodes for the home page of the project
+const nav = d3.select("#nav");
+const home = d3.select(".cover");
+const intro = d3.select(".intro");
 const questions = d3.select('#questions');
+const footer = d3.select("footer");
 
-Promise.all([
+if (questions.size() > 0) {
+  Promise.all([
     d3.text('info.yml'),
     d3.text('questions.yml'),
   ])
   .then(([info, questionsData]) =>  {
     info = jsyaml.load(info);
     questionsData = jsyaml.load(questionsData);
+
+    const cover = home.selectAll('div').data([info]).enter().append('div');
+
+    cover.append("h1").text(d => d.title)
+    .classed("cover__title", true);
+
+    cover.append("div")
+    .classed("authors", true)
+    .selectAll("span")
+    .data(d => d.authors)
+    .join("span")
+    .text(d => d.name);
+
+    const introText = intro.selectAll("div").data([info]).enter().append("div");
+    introText.append("p").text(d => d.description);
 
     const question = questions.selectAll('div').data(questionsData).enter().append('div');
     question.append('h1').text(d=>d.title);
@@ -16,3 +36,4 @@ Promise.all([
     question.append('img').attr('src',d=>`./${d.folder}/${d.cover}`);
 
   });
+};
